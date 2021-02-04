a5a1ea3a33a719849a83eeeb29bf0efe830a4f2e
Caro407 <caro.lefevre407@hotmail.fr>
2021-02-04 15:18:57 +0100

    Dog, DogSitter & Stroll models added. Seed generated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+30.times do |i|
+  Dog.create(name: Faker::Creature::Dog.name, age: rand(1..16))
+  DogSitter.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, age: rand(16..60))
+  Stroll.create(dog: Dog.all.sample, dog_sitter: DogSitter.all.sample)
+end
